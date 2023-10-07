86d272a84e732b02c56a71d221178bae44e1c974
NICOLAS CORREA <134348375+NICOLA5CORREA@users.noreply.github.com>
2023-10-07 12:00:32 -0500

    process.env1

--- src/config/config.js
+++ src/config/config.js
@@ -17,10 +17,10 @@ module.exports=
     "dialect": "mysql"
   },
   production: {
-    username: proces.env.DB_USER,
-    password: proces.env.DB_PASSWORD,
-    database: proces.env.DB_NAME,
-    host: proces.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "postgres",
     logging: false,
